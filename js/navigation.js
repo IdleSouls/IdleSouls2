3898d41c87caf5d296d1ca3d5274a4f57c0ad209
IdleSouls <matteo_palombo@hotmail.it>
2025-09-01 18:30:00 +0200

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -2,33 +2,33 @@ document.addEventListener("DOMContentLoaded", () => {
     const buttons = document.querySelectorAll(".navButton");
     const mainContent = document.getElementById("mainContent");
 
-    function loadSection(section) {
+    function loadSection(section){
         fetch(`pages/${section}.html`)
-            .then(res => res.text())
-            .then(html => {
+            .then(res=>res.text())
+            .then(html=>{
                 mainContent.innerHTML = html;
 
-                // Sezione meditation
+                // Se la sezione è meditation
                 const focusButton = document.getElementById('focusButton');
-                if (focusButton) {
-                    focusButton.addEventListener('mousedown', () => {
-                        if (!window.focusState.draining && !window.focusState.filling) window.focusState.filling = true;
+                if(focusButton){
+                    focusButton.addEventListener('mousedown',()=>{
+                        if(!window.focusState.filling && !window.focusState.draining)
+                            window.focusState.filling=true;
                     });
-                    focusButton.addEventListener('mouseup', () => { window.focusState.filling = false; });
-                    focusButton.addEventListener('mouseleave', () => { window.focusState.filling = false; });
+                    focusButton.addEventListener('mouseup',()=>window.focusState.filling=false);
+                    focusButton.addEventListener('mouseleave',()=>window.focusState.filling=false);
                 }
 
-                // Sezione upgrades
-                if (typeof window.renderUpgrades === 'function') {
+                // Se la sezione è upgrades
+                if(section==='upgrades'){
                     window.renderUpgrades();
                 }
 
                 window.updateProbabilitiesUI();
             })
-            .catch(err => console.error(err));
+            .catch(err=>console.error(err));
     }
 
-    buttons.forEach(btn => btn.addEventListener("click", () => loadSection(btn.dataset.section)));
-
-    loadSection("meditation"); // sezione iniziale
+    buttons.forEach(btn=>btn.addEventListener('click',()=>loadSection(btn.dataset.section)));
+    loadSection("meditation");
 });
